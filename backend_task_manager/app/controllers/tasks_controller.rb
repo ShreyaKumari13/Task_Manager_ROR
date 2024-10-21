3bac6f338d251be9cbd73af010ed05088d039b48
ShreyaKumari13 <shreyakumari2713@gmail.com>
2024-10-21 22:24:12 +0530

    initial commit

--- backend_task_manager/app/controllers/tasks_controller.rb
+++ backend_task_manager/app/controllers/tasks_controller.rb
@@ -1,13 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:update, :destroy]
 
-  # GET /tasks
   def index
     tasks = Task.all
     render json: tasks
   end
 
-  # POST /tasks
   def create
     task = Task.new(task_params)
     if task.save
@@ -17,7 +15,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PUT /tasks/:id
   def update
     if @task.update(task_params)
       render json: @task
@@ -26,7 +23,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/:id
   def destroy
     @task.destroy
     head :no_content
